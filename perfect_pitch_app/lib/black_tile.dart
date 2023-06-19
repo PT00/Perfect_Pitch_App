6c6434d81a35a0207697367f7b5a8f4f007aa6e1
Piotr Thamm <thammpiotr@gmail.com>
2023-06-19 23:12:14 +0200

    Piano keys made to be interactive

--- perfect_pitch_app/lib/black_tile.dart
+++ perfect_pitch_app/lib/black_tile.dart
@@ -1,59 +1,111 @@
 import 'package:flutter/material.dart';
 import 'package:perfect_pitch_app/custom_page_route.dart';
 import 'package:perfect_pitch_app/game_page.dart';
+import 'package:perfect_pitch_app/end_menu.dart';
 
-import 'end_menu.dart';
+class BlackTile extends StatefulWidget {
+  final BuildContext context;
+  final String tone;
+  final double position;
+  final double whiteWidth;
+  final double height;
+  final int index;
+  final int correctAnswer;
+  final int round;
+  final int score;
 
-Widget blackTile(
-    BuildContext context,
-    String tone,
-    double position,
-    double whiteWidth,
-    double height,
-    int index,
-    int correctAnswer,
-    int round,
-    int score) {
-  double blackWidth = whiteWidth * 0.60;
-  return Positioned(
-    top: 0,
-    left: position * whiteWidth - blackWidth / 2,
-    width: blackWidth,
-    height: height,
-    child: GestureDetector(
-      onTap: () {
-        if (round - 1 == 0) {
-          Navigator.of(context).push(CustomPageRoute(
-              builder: ((context) => EndMenu(
-                    score: correctAnswer == index ? score + 1 : score,
-                  ))));
-        } else if (correctAnswer == index) {
-          Navigator.of(context).push(CustomPageRoute(
-              builder: ((context) =>
-                  GamePage(score: score + 1, round: round - 1))));
-        } else {
-          Navigator.of(context).push(CustomPageRoute(
-              builder: ((context) =>
-                  GamePage(score: score, round: round - 1))));
-        }
-      },
-      child: Container(
-        decoration: BoxDecoration(
+  BlackTile({
+    required this.context,
+    required this.tone,
+    required this.position,
+    required this.whiteWidth,
+    required this.height,
+    required this.index,
+    required this.correctAnswer,
+    required this.round,
+    required this.score,
+  });
+
+  @override
+  _BlackTileState createState() => _BlackTileState();
+}
+
+class _BlackTileState extends State<BlackTile> {
+  bool isPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    double blackWidth = widget.whiteWidth * 0.60;
+    return Positioned(
+      top: 0,
+      left: widget.position * widget.whiteWidth - blackWidth / 2,
+      width: blackWidth,
+      height: widget.height,
+      child: GestureDetector(
+        onTapDown: (_) {
+          setState(() {
+            isPressed = true;
+          });
+        },
+        onTapUp: (_) {
+          setState(() {
+            isPressed = false;
+          });
+          if (widget.round - 1 == 0) {
+            Navigator.of(widget.context).push(
+              CustomPageRoute(
+                builder: ((context) => EndMenu(
+                      score: widget.correctAnswer == widget.index
+                          ? widget.score + 1
+                          : widget.score,
+                    )),
+              ),
+            );
+          } else if (widget.correctAnswer == widget.index) {
+            Navigator.of(widget.context).push(
+              CustomPageRoute(
+                builder: ((context) => GamePage(
+                      score: widget.score + 1,
+                      round: widget.round - 1,
+                    )),
+              ),
+            );
+          } else {
+            Navigator.of(widget.context).push(
+              CustomPageRoute(
+                builder: ((context) => GamePage(
+                      score: widget.score,
+                      round: widget.round - 1,
+                    )),
+              ),
+            );
+          }
+        },
+        onTapCancel: () {
+          setState(() {
+            isPressed = false;
+          });
+        },
+        child: Container(
+          decoration: BoxDecoration(
             borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20)),
-            color: Colors.black),
-        child: Align(
-          alignment: Alignment.bottomCenter,
-          child: Padding(
-            padding: const EdgeInsets.only(bottom: 16.0),
-            child: Text(
-              tone,
-              style: const TextStyle(color: Colors.white),
+              bottomLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20),
+            ),
+            color: isPressed ? Colors.grey[800] : Colors.black,
+          ),
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: Text(
+                widget.tone,
+                style: const TextStyle(color: Colors.white),
+              ),
             ),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
--- perfect_pitch_app/lib/white_tile.dart
+++ perfect_pitch_app/lib/white_tile.dart
@@ -1,44 +1,105 @@
-// ignore_for_file: avoid_types_as_parameter_names, non_constant_identifier_names
-
 import 'package:flutter/material.dart';
 import 'package:perfect_pitch_app/custom_page_route.dart';
 import 'package:perfect_pitch_app/end_menu.dart';
 import 'package:perfect_pitch_app/game_page.dart';
 
-Widget whiteTile(BuildContext context, String tone, double position,
-    double whiteWidth, int index, int correctAnswer, int round, int score) {
-  return Positioned(
-    top: 0,
-    left: position * whiteWidth,
-    width: whiteWidth,
-    bottom: 0,
-    child: GestureDetector(
-      onTapUp: (TapUpDetails) {
-        if (round - 1 == 0) {
-          Navigator.of(context).push(CustomPageRoute(
-              builder: ((context) => EndMenu(
-                    score: correctAnswer == index ? score + 1 : score,
-                  ))));
-        } else if (correctAnswer == index) {
-          Navigator.of(context).push(CustomPageRoute(
-              builder: ((context) =>
-                  GamePage(score: score + 1, round: round - 1))));
-        } else {
-          Navigator.of(context).push(CustomPageRoute(
-              builder: ((context) =>
-                  GamePage(score: score, round: round - 1))));
-        }
-      },
-      child: Container(
-        decoration: BoxDecoration(
-            color: Color.fromARGB(255, 223, 235, 245),
-            border: Border.all(color: Color.fromARGB(221, 26, 89, 216))),
-        child: Align(
-          alignment: Alignment.bottomCenter,
-          child: Padding(
-              padding: const EdgeInsets.only(bottom: 30.0), child: Text(tone)),
+class WhiteTile extends StatefulWidget {
+  final BuildContext context;
+  final String tone;
+  final double position;
+  final double whiteWidth;
+  final int index;
+  final int correctAnswer;
+  final int round;
+  final int score;
+
+  WhiteTile({
+    required this.context,
+    required this.tone,
+    required this.position,
+    required this.whiteWidth,
+    required this.index,
+    required this.correctAnswer,
+    required this.round,
+    required this.score,
+  });
+
+  @override
+  _WhiteTileState createState() => _WhiteTileState();
+}
+
+class _WhiteTileState extends State<WhiteTile> {
+  bool isPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: 0,
+      left: widget.position * widget.whiteWidth,
+      width: widget.whiteWidth,
+      bottom: 0,
+      child: GestureDetector(
+        onTapDown: (_) {
+          setState(() {
+            isPressed = true;
+          });
+        },
+        onTapUp: (_) {
+          setState(() {
+            isPressed = false;
+          });
+          if (widget.round - 1 == 0) {
+            Navigator.of(widget.context).push(
+              CustomPageRoute(
+                builder: ((context) => EndMenu(
+                      score: widget.correctAnswer == widget.index
+                          ? widget.score + 1
+                          : widget.score,
+                    )),
+              ),
+            );
+          } else if (widget.correctAnswer == widget.index) {
+            Navigator.of(widget.context).push(
+              CustomPageRoute(
+                builder: ((context) => GamePage(
+                      score: widget.score + 1,
+                      round: widget.round - 1,
+                    )),
+              ),
+            );
+          } else {
+            Navigator.of(widget.context).push(
+              CustomPageRoute(
+                builder: ((context) => GamePage(
+                      score: widget.score,
+                      round: widget.round - 1,
+                    )),
+              ),
+            );
+          }
+        },
+        onTapCancel: () {
+          setState(() {
+            isPressed = false;
+          });
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            color: isPressed
+                ? Color.fromARGB(
+                    255, 191, 211, 227) // Darker color when pressed
+                : Color.fromARGB(255, 223, 235, 245),
+            border: Border.all(color: Color.fromARGB(221, 26, 89, 216)),
+          ),
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 30.0),
+              child: Text(widget.tone),
+            ),
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
